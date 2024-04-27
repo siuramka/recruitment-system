9e8fa6579c827f3adc29c5007e69f0bdbbdb069f
siuramka <marius.kamarauskas9999@gmail.com>
2024-04-27 13:31:27 +0300

    tests

--- RecruitmentSystem/RecruitmentSystem.Tests/Services/AuthServiceTest.cs
+++ RecruitmentSystem/RecruitmentSystem.Tests/Services/AuthServiceTest.cs
@@ -126,5 +126,163 @@ public class AuthServiceTest
         
         Assert.That(result, Is.True);
     }
+    
+    [Test]
+    public void AuthorizeApplicationCompany_ShouldNotAuthorize()
+    {
+        var user = new SiteUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "User1",
+            CompanyId = Guid.NewGuid(),
+        };
+
+        _userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new SiteUser());
+        
+        var application = new Application()
+        {
+            Id = Guid.NewGuid(),
+            SiteUser = new SiteUser(),
+            InternshipId = Guid.NewGuid(),
+        };
+        
+        var internship = new Internship()
+        {
+            Id = application.InternshipId,
+            CompanyId = (Guid)user.CompanyId,
+        };
+        
+        _db.Setup(x => x.Applications).ReturnsDbSet(new List<Application> { application });
+        
+        _db.Setup(x => x.Internships).ReturnsDbSet(new List<Internship> { internship });
+        
+        var result = _authService.AuthorizeApplicationCompany(application.Id, user.Id).Result;
+        
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task AuthorizeApplicationCreator_ShouldAuthorize()
+    {
+        var user = new SiteUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "User1",
+            CompanyId = Guid.NewGuid(),
+        };
+
+        _userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
+        
+        var application = new Application()
+        {
+            Id = Guid.NewGuid(),
+            SiteUser = user,
+            InternshipId = Guid.NewGuid(),
+        };
+        
+        _db.Setup(x => x.Applications).ReturnsDbSet(new List<Application> { application });
+        
+        var result = await _authService.AuthorizeApplicationCreator(application.Id, user.Id);
+        
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    public async Task AuthorizeApplicationCreator_ShouldNotAuthorize()
+    {
+        var user = new SiteUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "User1",
+            CompanyId = Guid.NewGuid(),
+        };
+
+        _userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(new SiteUser());
+        
+        var application = new Application()
+        {
+            Id = Guid.NewGuid(),
+            SiteUser = user,
+            InternshipId = Guid.NewGuid(),
+        };
+        
+        _db.Setup(x => x.Applications).ReturnsDbSet(new List<Application> { application });
+        
+        var result = await _authService.AuthorizeApplicationCreator(application.Id, user.Id);
+        
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public async Task AuthorizeEvaluationCreator_ShouldAuthorize()
+    {
+        var user = new SiteUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "User1",
+            CompanyId = Guid.NewGuid(),
+        };
+
+        _userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
+        
+        var application = new Application()
+        {
+            Id = Guid.NewGuid(),
+            SiteUser = user,
+            InternshipId = Guid.NewGuid(),
+        };
+        
+        var evaluation = new Evaluation()
+        {
+            Id = Guid.NewGuid(),
+            Application = application,
+        };
+        
+        _db.Setup(x => x.Evaluations).ReturnsDbSet(new List<Evaluation> { evaluation });
+        
+        var result = await _authService.AuthorizeEvaluationCreator(evaluation.Id, user.Id);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public async Task AuthorizeEvaluationCompany_ShouldAuthorize()
+    {
+        var user = new SiteUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "User1",
+            CompanyId = Guid.NewGuid(),
+        };
+
+        _userManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(user);
+
+        var internship = new Internship()
+        {
+            Id = Guid.NewGuid(),
+            CompanyId = (Guid)user.CompanyId,
+        };
+
+        var application = new Application()
+        {
+            Id = Guid.NewGuid(),
+            SiteUser = user,
+            InternshipId = internship.Id,
+            Internship = internship
+        };
+
+        var evaluation = new Evaluation()
+        {
+            Id = Guid.NewGuid(),
+            ApplicationId = application.Id,
+            Application = application,
+        };
+
+        _db.Setup(x => x.Evaluations).ReturnsDbSet(new List<Evaluation> { evaluation });
+
+        var result = await _authService.AuthorizeEvaluationCompany(evaluation.Id, user.Id);
+
+        Assert.That(result, Is.True);
+    }
 }
 
